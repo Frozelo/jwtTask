cca51949aee7b31b4052d5441f113f5cef8f7321
ivansizov <sizovivan.05@gmail.com>
2024-12-13 16:47:50 +0700

    add retrieve payload info

--- main.go
+++ main.go
@@ -1,25 +1,52 @@
 package main
 
 import (
+	"fmt"
 	"github.com/golang-jwt/jwt/v5"
-	"log"
 	"time"
 )
 
-func main() {
+type Payload struct {
+	UserId   uint   `json:"userId"`
+	Email    string `json:"email"`
+	ClientIp string `json:"clientIp"`
+	jwt.RegisteredClaims
+}
 
-	userId := "123213213"
-	clientUp := "127.0.0.1"
-	payload := jwt.MapClaims{
-		"id":  userId,
-		"ip":  clientUp,
-		"exp": time.Now().Add(time.Hour * 24).Unix(),
+func main() {
+	expDate := time.Now().Add(1 * time.Hour)
+	payload := &Payload{
+		UserId:   1,
+		Email:    "test@gmail.com",
+		ClientIp: "127.0.0.1",
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(expDate),
+		},
 	}
 
-	token := jwt.NewWithClaims(jwt.SigningMethodHS512, payload)
-	signedToken, err := token.SignedString([]byte("very-very-secret"))
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, payload)
+	tokenString, err := token.SignedString([]byte("very-very-secret"))
 	if err != nil {
 		panic(err)
 	}
-	log.Println(signedToken)
+
+	payloadInfo, err := getPayloadByToken(tokenString)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(payloadInfo)
+
+}
+
+func getPayloadByToken(tokenString string) (*Payload, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &Payload{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte("very-very-secret"), nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	if payload, ok := token.Claims.(*Payload); ok && token.Valid {
+		return payload, nil
+	}
+	return nil, fmt.Errorf("invalid token")
 }
