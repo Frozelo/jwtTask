be002f54ec997f855311edfa9c9c6a22934386f5
ivansizov <sizovivan.05@gmail.com>
2024-12-14 00:17:56 +0700

    add refresh token hashing

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/Frozelo/jwtTask
 go 1.22.5
 
 require github.com/golang-jwt/jwt/v5 v5.2.1
+
+require golang.org/x/crypto v0.31.0 // indirect
--- main.go
+++ main.go
@@ -4,9 +4,9 @@ import (
 	"crypto/rand"
 	"encoding/base64"
 	"fmt"
-	"time"
 
 	"github.com/golang-jwt/jwt/v5"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type Payload struct {
@@ -17,44 +17,35 @@ type Payload struct {
 }
 
 func main() {
-	expDate := time.Now().Add(1 * time.Hour)
-	payload := &Payload{
-		UserId:   1,
-		Email:    "test@gmail.com",
-		ClientIp: "127.0.0.1",
-		RegisteredClaims: jwt.RegisteredClaims{
-			ExpiresAt: jwt.NewNumericDate(expDate),
-		},
-	}
-
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, payload)
-	tokenString, err := token.SignedString([]byte("very-very-secret"))
-	if err != nil {
-		panic(err)
-	}
-
-	payloadInfo, err := getPayloadByToken(tokenString)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(payloadInfo)
-
 	refreshToken, err := generateRefreshToken()
 	if err != nil {
 		panic(err)
 	}
+	hashRefreshToken, err := cryptRefreshToken(refreshToken)
+	if err != nil {
+		panic(err)
+	}
 
-	fmt.Println(refreshToken)
+	fmt.Printf("the standart refresh token is %s, the crypt refresh token is %s", refreshToken, hashRefreshToken)
 }
 
 func generateRefreshToken() (string, error) {
-	refreshBytes := make([]byte, 128)
+	refreshBytes := make([]byte, 32)
 	if _, err := rand.Read(refreshBytes); err != nil {
 		return "", err
 	}
 
 	refreshToken := base64.StdEncoding.EncodeToString(refreshBytes)
 	return refreshToken, nil
+}
+
+func cryptRefreshToken(refreshToken string) (string, error) {
+	hashRefreshToken, err := bcrypt.GenerateFromPassword([]byte(refreshToken), bcrypt.DefaultCost)
+	if err != nil {
+		return "", nil
+	}
+
+	return string(hashRefreshToken), nil
 }
 
 func getPayloadByToken(tokenString string) (*Payload, error) {
